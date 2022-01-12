d7345936447f4de1609aadf55354bdfd6ebda012
Kate Kiatsiri <keidsiri@gmail.com>
2022-01-12 09:35:52 -0800

    Update Copy.cs
    Co-authored-by: Hannah Young <hannah@corgibyte.com>
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>

--- Library/Models/Patron.cs
+++ Library/Models/Patron.cs
@@ -1,16 +1,16 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
-// namespace Library.Models
-// {
-//   public class Patron
-//   {
-//     public Patron()
-//     {
-//       this.Checkouts = new HashSet<Checkout>();
-//     }
+namespace Library.Models
+{
+  public class Patron
+  {
+    public Patron()
+    {
+      this.Checkouts = new HashSet<Checkout>();
+    }
 
-//     public int PatronId { get; set; }
-//     public string Name { get; set; }
-//     public virtual ICollection<Checkout> Checkouts { get; set; }
-//   }
-// }
+    public int PatronId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Checkout> Checkouts { get; set; }
+  }
+}
