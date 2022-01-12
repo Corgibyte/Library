bbba39aaacf56c788f56a8f6eb184293378ad982
Kate Kiatsiri <keidsiri@gmail.com>
2022-01-12 13:03:46 -0800

    Update Checkout
    Co-authored-by: Hannah Young <hannah@corgibyte.com>
    Co-Authored-by: Zach Wilson <zwilson22495@gmail.com>

--- Library/Controllers/CopiesController.cs
+++ Library/Controllers/CopiesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Library.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
@@ -14,9 +15,11 @@ namespace Library.Controllers
   public class CopiesController : Controller
   {
     private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public CopiesController(LibraryContext db)
+    public CopiesController(UserManager<ApplicationUser> userManager, LibraryContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
@@ -33,6 +36,25 @@ namespace Library.Controllers
       _db.Copies.Remove(thisCopy);
       _db.SaveChanges();
       return RedirectToAction("Details", "Books", new { id = thisCopy.BookId });
+    }
+
+    [Authorize]
+    public ActionResult CheckoutCopy(int id)
+    {
+      return View(_db.Copies.FirstOrDefault(copy => copy.CopyId == id));
+    }
+
+    [Authorize, HttpPost]
+    public async Task<ActionResult> CheckoutCopy(Copy copy)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      DateTime dueDate = DateTime.Now + new TimeSpan(14, 0, 0, 0); 
+      Checkout newCheckout = new Checkout() { CopyId = copy.CopyId, IsReturned = false, DueDate = dueDate };
+      newCheckout.User = currentUser;
+      _db.Checkouts.Add(newCheckout);
+      _db.SaveChanges();
+      return RedirectToAction("Details", "Books", new { id = copy.Book.BookId });
     }
   }
 }
--- Library/Models/Checkout.cs
+++ Library/Models/Checkout.cs
@@ -1,14 +1,17 @@
 using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Models
 {
   public class Checkout
   {
     public int CheckoutId { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
     public DateTime DueDate { get; set; }
     public bool IsReturned { get; set; }
     public int CopyId { get; set; }
     public virtual Copy Copy { get; set; }
+    public virtual ApplicationUser User { get; set; }
   }
 }
